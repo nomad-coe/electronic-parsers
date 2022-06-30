The parsers for VASP and FHIaims incorrectly rescale by volume (in an attempt to remove that dependency) However, their units are off and so this rescaling has been removed now
Note: Accidentally removed old commits before pushing (was waiting for a confirmation)
This is a manual recuperation

Changes to be committed:
	modified:   electronicparsers/fhiaims/parser.py
	modified:   electronicparsers/vasp/parser.py
	new file:   tests/test_issue_868/example_git_issue_868.py

electronicparsers/fhiaims/parser.py
@@ -840,11 +840,6 @@ class FHIAimsParser:
             sec_scc = sec_run.calculation[-1]
             sec_dos = None
             energies = None
-            lattice_vectors = section.get(
-                'lattice_vectors', self.out_parser.get('lattice_vectors'))
-            if lattice_vectors is None:
-                lattice_vectors = np.eye(3) * ureg.angstrom
-            volume = np.abs(np.linalg.det(lattice_vectors.magnitude)) * ureg.angstrom ** 3
             n_spin = self.out_parser.get_number_of_spin_channels()
             # parse total first, we expect only one file
             total_dos_files, _ = section.get('total_dos_files', [['KS_DOS_total_raw.dat'], []])
@@ -857,7 +852,7 @@ class FHIAimsParser:
                 sec_dos.n_energies = len(energies)
                 sec_dos.energies = energies
                 # dos unit is 1/(eV-cell volume)
-                dos = data[1: n_spin + 1] * (1 / ureg.eV) * volume.to('m**3').magnitude
+                dos = data[1: n_spin + 1] / ureg.eV
                 for spin in range(len(dos)):
                     sec_dos_values = sec_dos.m_create(DosValues, Dos.total)
                     sec_dos_values.spin = spin

electronicparsers/vasp/parser.py
@@ -613,9 +613,7 @@ class OutcarContentParser(ContentParser):
         # DOSCAR fomat (spin) energy dos_up dos_down integrated_up integrated_down
         dos = np.transpose(dos)
         dos_energies = dos[0]
-        cell = self.get_structure(n_calc)['cell']
-        volume = np.abs(np.linalg.det(cell.magnitude))
-        dos_values = dos[1: 1 + self.ispin] * volume
+        dos_values = dos[1: 1 + self.ispin]
         dos_integrated = dos[1 + self.ispin: 2 * self.ispin + 1]
 
         return dos_energies, dos_values, dos_integrated, e_fermi
@@ -1086,11 +1084,6 @@ class RunContentParser(ContentParser):
         dos_energies = dos[0].T[0]
         dos_values = dos[1].T
         dos_integrated = dos[2].T
-
-        # unit of dos in vasprun is states/eV/cell
-        cell = self.get_structure(n_calc)['cell']
-        volume = np.abs(np.linalg.det(cell.magnitude)) * ureg.angstrom ** 3
-        dos_values *= volume.to('m**3').magnitude
 
         return dos_energies, dos_values, dos_integrated, e_fermi
 