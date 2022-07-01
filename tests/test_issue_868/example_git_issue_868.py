6660edee810af42e0b47a8cd80a63348a9007997
Nathan Daelman <ndaelman@physik.hu-berlin.de>
2022-07-01 13:10:01 +0200

    Cleaned up example analysis and manually enforced alignment to the Fermi level
    Changes to be committed:
    	modified:   example_git_issue_868.py

--- tests/test_issue_868/example_git_issue_868.py
+++ tests/test_issue_868/example_git_issue_868.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import json
 import numpy as np
 import pandas as pd
@@ -9,32 +9,33 @@ from scipy import integrate
 # set constants
 ev_to_joule = 1.602177e-19
 window = 100
-prefix = '/home/nathan/Documents/electronic-parsers/tests/test_issue_868/'
 example_files = ('fhi', 'vasp')
-#%%
-# read in data and store it in a dataframe or a dictionary
+# %%
+# Read in data and store it in a dataframe or a dictionary
 df = pd.DataFrame({})
-normalization_factors = {}
 
 for filename in example_files:
     program = filename.upper()
-    fp = open(prefix + filename + '.json')
+    fp = open(filename + '.json')
     data = json.load(fp)
     dos = data['run'][0]['calculation'][0]['dos_electronic'][0]
     fp.close()
     n_entries = len(dos['total'][0]['value'])
-    marked_steps = int(n_entries / 1000)  # WARNING: parameter might need adjusting
+    marked_steps = int(n_entries / 1000)  # WARNING: parameter set manually might need adjusting
     df['d' + program] = [x for i, x in enumerate(dos['total'][0]['value']) if (i % marked_steps) == 0]
     df['r' + program] = df['d' + program].rolling(window).mean().fillna(0)
     df['e' + program] = [x for i, x in enumerate(dos['energies']) if (i % marked_steps) == 0]
-    df['e' + program] = df['e' + program] - dos['energy_fermi']
-    normalization_factors[program] = dos['total'][0]['normalization_factor']
-#%%
+    df['e' + program] -= dos['energy_fermi']
+
+# Fermi energy for FHIaims version 210716 not properly extracted
+df['eFHI'] -= -6.15211356 * ev_to_joule
+# %%
+# Plot raw (unaligned) DOS
 fig_fhi = px.line(df, x='eFHI', y='dFHI')
 fig_vasp = px.line(df, x='eVASP', y='dVASP')
 fig = go.Figure(data=fig_fhi.data + fig_vasp.data)
 fig.show()
-
+# %%
 # Plot rolling averages
 fig_fhi = px.line(df, x='eFHI', y='rFHI')
 fig_fhi.data[0]['line']['color'] = 'orange'
@@ -46,34 +47,12 @@ fig_vasp.data[0]['hovertemplate'] = 'VASP<br>eVASP=%{x}<br>rVASP=%{y}<extra></ex
 fig = go.Figure(data=fig_fhi.data + fig_vasp.data)
 fig.update_layout(title='Rolling average (window = ' + str(window) + ' pts)')
 fig.show()
-#%%
-# Select comparison energy range
-trimmed_axis = df.loc[(df['rVASP'] > 1e-10) & (df['rFHI'] > 1e-10), ['eFHI', 'eVASP']].dropna(axis=0)
-lower = trimmed_axis.min().min()
-upper = trimmed_axis.max().max()
-
-# Define a common energy axis, bin the dos and average over bin
-df['energy'] = pd.Series(np.linspace(lower, upper, df.shape[0]))
-for filename in example_files:
-    program = filename.upper()
-    temp = [df.loc[(x < df['e' + program]) & (df['e' + program] < y), 'r' + program].tolist()
-            for x, y in zip(df['energy'], df['energy'].shift(-1))]
-    df['s' + program] = pd.DataFrame(temp).mean(axis=1, skipna=True)
-
-# Compute and plot ratio
-df['VASP/FHI'] = df['sVASP']/df['sFHI']
-df_plot = df.dropna()
-fig = px.line(df_plot, 'energy', 'VASP/FHI')
-normalization_ratio = normalization_factors['VASP'] / normalization_factors['FHI']
-fig.add_trace(go.Scatter(x=[df_plot['energy'].min(), df_plot['energy'].max()],
-              y=[normalization_ratio, normalization_ratio], name='norm_factor ratio'))
-fig.show()
-#%%
+# %%
+# Compute the integral (#valence electrons and the ratio)
 dos_integral = {}
 for filename in example_files:
     program = filename.upper()
-    dos_integral[program] = integrate.trapz(df.loc[df['e' + program] < 0, 'd' + program], df.loc[df['e'+program] < 0, 'e'+program])
-print(dos_integral['FHI'], dos_integral['VASP'], dos_integral['VASP'] / dos_integral['FHI'])
-#%%
-df
-# %%
+    dos_integral[program] = integrate.trapz(df.loc[df['e' + program] < 0, 'd' + program], df.loc[df['e' + program] < 0, 'e' + program])
+print('FHI: ' + str(dos_integral['FHI']))
+print('VASP:' + str(dos_integral['VASP']))
+print('VASP/FHI' + str(dos_integral['VASP'] / dos_integral['FHI']))
