1a830fa9ae0280327f26888de4963ab4c272eb39
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2022-11-03 14:59:55 +0100

    > Amending pylint Wannier90 > New tests > LatticeModel in the System section

--- electronicparsers/alf/parser.py
+++ electronicparsers/alf/parser.py
@@ -20,8 +20,8 @@ import numpy as np
 import os
 import logging
 import h5py
-import re
 
+from nomad.units import ureg
 from nomad.parsing.file_parser import TextParser, Quantity
 from nomad.datamodel.metainfo.simulation.run import Run, Program
 from nomad.datamodel.metainfo.simulation.calculation import (
@@ -31,6 +31,7 @@ from nomad.datamodel.metainfo.simulation.method import Method, QMC
 from nomad.datamodel.metainfo.simulation.system import (
     System, Atoms
 )
+from nomad.datamodel.metainfo.workflow import Workflow
 
 from .metainfo.alf import (
     x_alf_parameters_var_lattice, x_alf_parameters_var_hubbard,
@@ -52,7 +53,7 @@ class InfoParser(TextParser):
 
         self._quantities = [
             Quantity(
-                'parameters', f'{re_n} *([\w.,# \w-]+ *: *.+)',
+                'parameters', rf'{re_n} *([\w.,# \w-]+ *: *.+)',
                 repeats=True, str_operation=str_to_params),
             Quantity(
                 'program_commit', r'commit\s*(\w+)',
@@ -83,6 +84,10 @@ class ALFParser:
             'Ndim': 'n_dim',
             'Norb': 'n_orb',
             'Orbital1': 'orbital',
+            'Orbital2': 'orbital',
+            'Orbital3': 'orbital',
+            'Orbital4': 'orbital',
+            'Orbital5': 'orbital',
             'a1': 'a1',
             'a2': 'a2'
         }
@@ -102,48 +107,60 @@ class ALFParser:
             }
         }
 
-    def parse_initial_model(self, data):
-        sec_run = self.archive.run[-1]
-        sec_scc = sec_run.m_create(Calculation)
-        #sec_hoppings = sec_scc.m_create(HoppingMatrix)
-
-        #sec_hoppings.n_orbitals = data['lattice'].attrs.get('Norb', None)
-
-        #sec_hoppings.n_wigner_seitz_points =
-        #sec_hoppings.value =
-
-        #sec_scc.n_references = 1
-        #sec_scc.calculations_ref = [sec_hoppings]
-
     def parse_system(self, data):
         sec_run = self.archive.run[-1]
         sec_system = sec_run.m_create(System)
 
+        norb = data['lattice'].attrs.get('Norb')
+        ndim = data['lattice'].attrs.get('Ndim')
+        orbital_position = np.zeros((ndim, norb))
         for keys in data['lattice'].attrs.keys():
-            setattr(sec_system, f'x_alf_{self._lattice_map[keys]}', data['lattice'].attrs[keys])
+            if 'Orbital' in keys:
+                np.append(orbital_position, data['lattice'].attrs.get(keys))
+            else:
+                setattr(sec_system, f'x_alf_{self._lattice_map[keys]}', data['lattice'].attrs.get(keys))
+        sec_system.x_alf_orbital = orbital_position
 
         sec_var_lattice = sec_system.m_create(x_alf_parameters_var_lattice)
         for keys in data['parameters']['var_lattice'].attrs.keys():
-            val = data['parameters']['var_lattice'].attrs[keys]
+            val = data['parameters']['var_lattice'].attrs.get(keys)
             if keys == 'lattice_type' or keys == 'model':
-                val = val.decode('UTF-8') # from binary data to str
+                val = val.decode('UTF-8')  # from binary data to str
             setattr(sec_var_lattice, f'x_alf_{keys}', val)
 
         # Parse the lattice model as system
         sec_system.name = sec_system.x_alf_var_lattice.x_alf_lattice_type
         sec_system.type = '2D'
+        sec_system.is_representative = True
         sec_atoms = sec_system.m_create(Atoms)
         sec_atoms.lattice_vectors = np.vstack((
-            np.append(sec_system.x_alf_a1, 0.0),
-            np.append(sec_system.x_alf_a2, 0.0),
-            np.array([0.0, 0.0, 1.0])))
-        sec_atoms.periodic = [sec_atoms.lattice_vectors is not None] * 3
-        sec_atoms.supercell_matrix = np.vstack((
-            np.append(sec_system.x_alf_l1, 0.0),
-            np.append(sec_system.x_alf_l2, 0.0),
-            np.array([0.0, 0.0, 1.0])))
-        #sec_atoms.labels
-        #sec_atoms.positions
+            np.append(sec_system.x_alf_a1, 0.0) * ureg.angstrom,
+            np.append(sec_system.x_alf_a2, 0.0) * ureg.angstrom,
+            np.array([0.0, 0.0, 4.0]) * ureg.angstrom))  # arbitrary Z direction def
+        sec_atoms.periodic = [True, True, False]
+        # QMC supercell definition
+        n_l1 = sec_system.x_alf_var_lattice.x_alf_l1
+        n_l2 = sec_system.x_alf_var_lattice.x_alf_l2
+        labels = ['X'] * (n_l1 * n_l2)
+        positions = [[[i / n_l1, j / n_l2, 0.0] for j in range(n_l2)] for i in range(n_l1)]
+        sec_atoms.labels = labels
+        sec_atoms.positions = np.vstack(positions * ureg.angstrom)
+
+    def parse_initial_model(self, data):
+        sec_run = self.archive.run[-1]
+
+        sec_scc = sec_run.m_create(Calculation)
+        sec_scc.method_ref = sec_run.method[-1]
+        sec_scc.system_ref = sec_run.system[-1]
+        sec_hoppings = sec_scc.m_create(HoppingMatrix)
+
+        # TODO populate after talking with Jefferson and Jonas
+        if sec_run.system is not None:
+            sec_hoppings.n_orbitals = sec_run.system[0].x_alf_n_orb
+
+            # hr = [x, y, z, orb1, orb2, ret]
+            # sec_hoppings.n_wigner_seitz_points =
+            # sec_hoppings.value = ...
 
     def parse_method(self, data):
         sec_run = self.archive.run[-1]
@@ -159,6 +176,7 @@ class ALFParser:
         sec_qmc = sec_method.m_create(QMC)
         # sec_qmc.t0 = sec_method.x_alf_var_hubbard.x_alf_ham_t
         sec_qmc.U = sec_method.x_alf_var_hubbard.x_alf_ham_u
+        sec_qmc.V = sec_method.x_alf_var_hubbard.x_alf_ham_u2
         sec_qmc.chemical_potential = sec_method.x_alf_var_hubbard.x_alf_ham_chem
         sec_qmc.inverse_temperature = sec_method.x_alf_var_model_generic.x_alf_beta
         sec_qmc.dtau = sec_method.x_alf_var_model_generic.x_alf_dtau
@@ -178,6 +196,9 @@ class ALFParser:
     def parse_scc(self, data):
         sec_run = self.archive.run[-1]
         sec_scc = sec_run.m_create(Calculation)
+        sec_scc.method_ref = sec_run.method[-1]
+        sec_scc.system_ref = sec_run.system[-1]
+        sec_scc.calculations_ref = [sec_run.calculation[0]]
 
         # Equilibrium and tau quantities
         for observable in self._observables_map['eqtau']:
@@ -233,97 +254,15 @@ class ALFParser:
             if branch:
                 sec_program.x_alf_commit_branch = branch
 
-        # reference to an input hopping model
-        self.parse_initial_model(data)
-
         self.parse_system(data)
 
         self.parse_method(data)
 
+        # reference to an input hopping model
+        self.parse_initial_model(data)
+
+        # QMC calculation
         self.parse_scc(data)
 
-
-        '''
-        sec_calc = sec_run.m_create(Calculation)
-
-        def parse_obser(source, target):
-            # observables have arbitrary shapes, need to iterate until we until 1-d
-            if len(source.shape) == 1:
-                target.x_alf_obser.append(x_alf_observable_data(x_afl_data=source))
-                return
-            for data_n in source:
-                parse_obser(data_n, target)
-
-        ### Calculation section
-        for observable in self._observables:
-            sec_observable = x_alf_observable()
-            data_observable = data[observable]
-            if 'sign' in data_observable.keys():
-                sec_observable.x_alf_sign = data_observable['sign']
-            if 'obser' in data_observable.keys():
-                sec_observable.x_alf_obser = data_observable['obser'][:]
-                # TODO determine if we need to flatten data
-                # parse_obser(data_observable['obser'][:], sec_observable)
-            # TODO parse other properties
-            setattr(sec_calc, f'x_alf_{observable.lower()}', sec_observable)
-
-        ### Method section
-        sec_method = sec_run.m_create(Method)
-        sec_input_parameters = x_alf_input_parameters()
-        for key in ['var_lattice', 'var_hubbard', 'var_model_generic']:
-            data_parameter = dict(data['parameters'].get(key).attrs.items())
-            setattr(sec_input_parameters, f'x_alf_{key}', data_parameter)
-            sec_method.x_alf_input_parameters = sec_input_parameters
-
-        # Decoding np.bytes to str
-        for key in ['lattice_type', 'model']:
-            val = sec_method.x_alf_input_parameters.x_alf_var_lattice.get(key, {})
-            sec_method.x_alf_input_parameters.x_alf_var_lattice.update({key : val.decode('UTF-8')})
-
-        ### System section
-        sec_system = sec_run.m_create(System)
-        data_system = dict(data['lattice'].attrs.items())
-        setattr(sec_system, f'x_alf_lattice', data_system)
-
-        sec_system.name = sec_method.x_alf_input_parameters.x_alf_var_lattice['lattice_type']
-        if sec_system.name in self._system_names and data_system['Ndim'] == 2:
-            sec_system.type = '2D'
-
-        sec_atoms = sec_system.m_create(Atoms)
-        sec_atoms.n_atoms = 1
-                            #(sec_method.x_alf_input_parameters.x_alf_var_lattice['l1'].item()) \
-                            #*(sec_method.x_alf_input_parameters.x_alf_var_lattice['l2'].item())
-        sec_atoms.lattice_vectors = np.array([data_system['a1'], data_system['a2']])#*e-10
-        sec_atoms.positions = np.zeros((sec_atoms.n_atoms, 3))
-        sec_atoms.supercell_matrix = np.zeros((3, 3))
-        for i in range(data_system['Ndim']):
-            sec_atoms.supercell_matrix[i][i] = sec_method.x_alf_input_parameters.x_alf_var_lattice[f'l{i+1}'].item()
-
-        sec_atoms_orbitals = sec_system.m_create(AtomsOrbitals)
-        sec_atoms_orbitals.n_orbitals = data_system['Norb']
-        sec_atoms_orbitals.positions = np.zeros((sec_atoms_orbitals.n_orbitals, 3))
-        for i in range(sec_atoms_orbitals.n_orbitals):
-            print(i, sec_atoms_orbitals.positions[i])
-            #print(type(data_system['Orbital1']))
-            #sec_orbitals.positions[i].magnitude = np.pad(data_system['Orbital1'], (0, 1))
-            print(sec_atoms_orbitals.positions[i].magnitude)
-
-        #sec_atoms_group = sec_system.m_create(AtomsGroup)
-        #sec_atoms_group.label = 'Unit cell'
-
-
-        #sec_lattice_geometry = sec_lattice_model.m_create(LatticeGeometry)
-        #sec_system.lattice_geometry.lattice_name = sec_method.x_alf_input_parameters.x_alf_var_lattice['lattice_type']
-
-        #for key_mapping in self._system_keys_mapping.keys():
-        #    print(key_mapping, self._system_keys_mapping[key_mapping])
-        #    print(sec_system.x_alf_lattice[key_mapping])
-            #if key_mapping == 'Orbital1':
-            #    sec_system.x_alf_lattice[key_mapping] = sec_system.x_alf_lattice[key_mapping]
-        #    setattr(sec_system.lattice_geometry, self._system_keys_mapping[key_mapping], sec_system.x_alf_lattice[key_mapping])
-
-        #sec_system.lattice_geometry.lattice_vectors = np.array([data_system['a1'], data_system['a2']])
-        #sec_system.lattice_geometry.Nsupercell = np.array([sec_method.x_alf_input_parameters.x_alf_var_lattice['l1'],
-        #                                    sec_method.x_alf_input_parameters.x_alf_var_lattice['l2']])
-        #sec_system.lattice_geometry.supercell_vectors = np.array([data_system['L1'], data_system['L2']])
-        '''
+        sec_workflow = archive.m_create(Workflow)
+        sec_workflow.type = 'single_point'
