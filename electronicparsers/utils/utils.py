15a3c132116954c2f2e72d5d2b0da2ac2de747aa
Jose Pizarro <jose.pizarro@physik.hu-berlin.de>
2023-02-27 11:25:03 +0100

    Modified extract_section to traverse through the list of path

--- electronicparsers/utils/utils.py
+++ electronicparsers/utils/utils.py
@@ -35,24 +35,20 @@ def extract_section(source, path):
         sec_system = extract_section(archive, 'run/system')
         spectra = extract_section(archive, 'run/calculation/spectra')
     '''
-    path_segments = path.split('/', 1)
-    try:
-        value = getattr(source, path_segments[0])
-        value = value[-1] if isinstance(value, list) else value
-    except Exception:
-        return
-
-    if len(path_segments) == 1:
-        return value
-    else:
-        return extract_section(value, path_segments[1])
+    section_segments = path.split('/')
+    for section in section_segments:
+        if source.m_xpath(section):
+            value = getattr(source, section)
+            source = value[-1] if isinstance(value, list) else value
+        else:
+            return
+    return source
 
 
 def get_files(pattern, filepath, stripname: str = '', deep: bool = True):
     '''
-    Get files in a recursive way (up ** or down ..) from the folder of the mainfile.
+    Get files up to / down from the filepath (up ** or down ..).
     '''
-    # TODO do recursion until files are found
     for _ in range(10):
         filenames = glob(f'{os.path.dirname(filepath)}/{pattern}')
         pattern = os.path.join('**' if deep else '..', pattern)
