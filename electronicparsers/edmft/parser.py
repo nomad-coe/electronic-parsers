dbe2ef7b22fa3e34b29f1bba764cf53a8fb7773a
Jose Pizarro <jose.pizarro@physik.hu-berlin.de>
2023-07-18 15:21:06 +0200

    Added method section

--- electronicparsers/edmft/parser.py
+++ electronicparsers/edmft/parser.py
@@ -28,14 +28,14 @@ from nomad.datamodel.metainfo.simulation.run import Run, Program
 from nomad.datamodel.metainfo.simulation.system import System, Atoms
 from nomad.datamodel.metainfo.simulation.method import (
     Method, HubbardKanamoriModel, LatticeModelHamiltonian, KMesh, FrequencyMesh, TimeMesh,
-    DMFT
+    DMFT, AtomParameters
 )
 from nomad.datamodel.metainfo.simulation.calculation import (
     Calculation, ScfIteration, Energy, GreensFunctions
 )
 from nomad.datamodel.metainfo.simulation.workflow import SinglePoint
 from ..utils import get_files
-from ..wien2k.parser import StructParser
+from ..wien2k.parser import StructParser  # Wien2k is imported to parse the system information
 
 
 class IndmflParser(TextParser):
@@ -45,8 +45,18 @@ class IndmflParser(TextParser):
     def init_quantities(self):
         self._quantities = [
             Quantity(
-                'version',
-                r'LABEL\d+\:\s*using WIEN2k_(\S+) \(Release ([\d\/]+)\)', flatten=False)]
+                'hybridization_window',
+                r'(\d+) *(\d+) *(\d+) *(\d+) *\# hybridization band index nemin and nemax\, renormalize for interstitials\, projection type'),
+            Quantity(
+                'real_or_imaginary_axis',
+                r'(\d+) *([\d\.]+) *([\d\.]+) *(\d+) *([\d\-\.]) *([\d\-\.]) *\# matsubara\, broadening\-corr\, broadening\-noncorr\, nomega\, omega\_min\, omega\_max \(in eV\)'),
+            Quantity(
+                'n_corr_atoms', r'(\d+)\s*\# number of correlated atoms'),
+            Quantity(
+                'i_atom_corr', r'(\d+)\s*(\d+)\s*(\d+)\s*\# iatom\, nL\, locrot', repeats=True),
+            Quantity(
+                'l_atom_corr', r'\s*(\d+)\s*(\d+)\s*(\d+)\s*\# L\, qsplit\, cix', repeats=True)
+        ]
 
 
 class ParamsParser(TextParser):
@@ -81,14 +91,17 @@ class EDMFTParser:
         self._re_namesafe = re.compile(r'[^\w]')
         self._calculation_type = 'dmft'
 
-        self.struct_parser = StructParser()
         self.indmfl_parser = IndmflParser()
+        self.struct_parser = StructParser()
         self.params_parser = ParamsParser()
 
         self._solver_map = {
             'CTQMC': 'CT-HYB',
-            'OCA': 'OCA'
+            'OCA': 'OCA',
+            'NCA': 'NCA'
         }
+
+        self._angular_momentum = ['s', 'p', 'd', 'f']
 
     def parse_system(self):
         struct_file = get_files('*.struct', self.filepath, self.mainfile)
@@ -108,63 +121,61 @@ class EDMFTParser:
             sec_atoms.periodic = atoms.get_pbc()
 
     def parse_initial_model(self):
-        '''
-        for n in range(data.attrs.get('general.nat', 1)):
-            sec_hubbard_kanamori_model = sec_hamiltonian.m_create(HubbardKanamoriModel)
+        sec_run = self.archive.run[-1]
+        sec_method = sec_run.m_create(Method)
 
-            angular_momentum = 'd'
-            sec_hubbard_kanamori_model.orbital = angular_momentum
-            sec_hubbard_kanamori_model.double_counting_correction = data.attrs.get('general.dc', None)
-            # w2dynamics keeps spin-rotational invariance
-            for key in self._hubbard_kanamori_map.keys():
-                parameters = data.attrs.get(f'atoms.{n+1}.{key}{angular_momentum}{angular_momentum}', None) * ureg.eV
-                setattr(sec_hubbard_kanamori_model, self._hubbard_kanamori_map.get(key), parameters)
-
-            if data.attrs.get(f'atoms.{n+1}.hamiltonian') == 'Density':
-                sec_hubbard_kanamori_model.j = 0.0
-            elif data.attrs.get(f'atoms.{n+1}.hamiltonian') == 'Kanamori':
-                sec_hubbard_kanamori_model.j = sec_hubbard_kanamori_model.jh
-        '''
+        # TODO ask Lucian what projectorw.dat means to parse as initial model.
+        # HubbardKanamori part
+        for n in range(self.indmfl_parser.get('n_corr_atoms', 1)):
+            sec_atom_params = sec_method.m_create(AtomParameters)
+            if sec_run.m_xpath('system[-1].atoms.labels'):
+                labels = sec_run.system[-1].atoms.labels
+                if self.indmfl_parser.get('i_atom_corr') is not None:
+                    atom_corr = self.indmfl_parser.get('i_atom_corr')[n]
+                    label = labels[atom_corr[0] - 1]
+                    sec_atom_params.label = label
+                    if self.indmfl_parser.get('l_atom_corr') is not None:
+                        angular_momentum = self._angular_momentum[self.indmfl_parser.get('l_atom_corr')[n][0]]
+            sec_hubbard_kanamori = sec_atom_params.m_create(HubbardKanamoriModel)
+            sec_hubbard_kanamori.double_counting_correction = self.general_parameters.get('DCs', '')
+            sec_hubbard_kanamori.u = self.impurity_parameters.get('U', 0.0) * ureg.eV
+            sec_hubbard_kanamori.jh = self.impurity_parameters.get('J', 0.0) * ureg.eV
+            if self.impurity_parameters.get('CoulombF', 'Full') == 'Ising':
+                sec_hubbard_kanamori.j = 0.0
+            else:
+                sec_hubbard_kanamori.up = sec_hubbard_kanamori.u - 2 * sec_hubbard_kanamori.jh
+                sec_hubbard_kanamori.j = sec_hubbard_kanamori.jh
 
     def parse_method(self):
         sec_run = self.archive.run[-1]
 
-        def parse_hubbard_model(method, gen_params, imp_params):
-            pass
+        sec_method = sec_run.m_create(Method)
+        sec_method.starting_method_ref = sec_run.method[0]  # ref to the Non- and InteractionHamiltonian
 
-        def parse_dmft(method, gen_params, imp_params):
-            sec_dmft = method.m_create(DMFT)
-            sec_dmft.inverse_temperature = imp_params.get('beta') / ureg.eV
-            sec_dmft.impurity_solver = self._solver_map.get(gen_params.get('solver'))
+        # Code-specific parameters
+        sec_method.x_edmft_general_parameters = self.general_parameters
+        sec_method.x_edmft_impurity_solver_parameters = self.impurity_parameters
 
-        def parse_meshes(method):
-            pass
-
-        params_file = get_files('*params.dat', self.filepath, self.mainfile)
-        if params_file:
-            if len(params_file) > 1:
-                self.logger.warning('Multiple *params.dat files found; we will parse the last one!')
-            self.params_parser.mainfile = params_file[-1]
-
-            gen_parameters = dict(self.params_parser.get('general_parameters', {}).get('params', []))
-            imp_parameters = dict(self.params_parser.get('impurity_parameters', {}).get('params', []))
-
-            sec_method = sec_run.m_create(Method)
-            sec_method.x_edmft_general_parameters = gen_parameters
-            sec_method.x_edmft_impurity_solver_parameters = imp_parameters
-            if gen_parameters and imp_parameters:
-                parse_hubbard_model(sec_method, gen_parameters, imp_parameters)
-                parse_dmft(sec_method, gen_parameters, imp_parameters)
-                parse_meshes(sec_method)
+        # DMFT method
+        sec_dmft = sec_method.m_create(DMFT)
+        n_corr_atoms = self.indmfl_parser.get('n_corr_atoms', 1)
+        sec_dmft.n_atoms_per_unit_cell = n_corr_atoms
+        # TODO add sec_dmft.n_correlated_orbitals
+        n_corr_elect = self.impurity_parameters.get('nf0') / n_corr_atoms
+        corr_elect = [n_corr_elect] * n_corr_atoms
+        sec_dmft.n_correlated_electrons = corr_elect
+        sec_dmft.inverse_temperature = self.impurity_parameters.get('beta', 0.0) / ureg.eV
+        sec_dmft.magnetic_state = 'paramagnetic'  # TODO ask Lucian if this is correct
+        sec_dmft.impurity_solver = self._solver_map.get(self.general_parameters.get('solver', ''))
 
     def parse_scc(self):
         pass
 
     def init_parser(self):
-        self.struct_parser.mainfile = None
-        self.struct_parser.logger = self.logger
         self.indmfl_parser.mainfile = self.mainfile
         self.indmfl_parser.logger = self.logger
+        self.struct_parser.mainfile = None
+        self.struct_parser.logger = self.logger
         self.params_parser.mainfile = None
         self.params_parser.logger = self.logger
 
@@ -177,6 +188,17 @@ class EDMFTParser:
 
         self.init_parser()
 
+        params_file = get_files('*params.dat', self.filepath, self.mainfile)
+        if params_file:
+            if len(params_file) > 1:
+                self.logger.warning(f'Multiple *params.dat files found; we will parse the last one: {params_file[-1]}')
+            self.params_parser.mainfile = params_file[-1]
+
+            if self.params_parser.get('general_parameters'):
+                self.general_parameters = dict(self.params_parser.get('general_parameters').get('params', []))
+            if self.params_parser.get('impurity_parameters'):
+                self.impurity_parameters = dict(self.params_parser.get('impurity_parameters').get('params', []))
+
         # Program section
         sec_run = self.archive.m_create(Run)
         sec_run.program = Program(name='eDMFT')
@@ -185,8 +207,9 @@ class EDMFTParser:
         self.parse_system()
 
         # Method.DMFT section
-        self.parse_initial_model()
-        self.parse_method()
+        if self.general_parameters and self.impurity_parameters:
+            self.parse_initial_model()
+            self.parse_method()
 
         # Calculation section
         self.parse_scc()
