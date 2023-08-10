Fixed units of greens_functions.chemical_potential to joule

electronicparsers/w2dynamics/parser.py
@@ -348,7 +348,7 @@ class W2DynamicsParser(BeyondDFTWorkflowsParser):
                 if sec_run.method[-1].m_xpath('time_mesh'):
                     sec_gf.tau = sec_run.method[-1].time_mesh.points.imag
                 if self.data.get(key).get('mu') is not None:
-                    sec_gf.chemical_potential = self.data.get(key).get('mu').get('value')
+                    sec_gf.chemical_potential = np.float64(self.data.get(key).get('mu').get('value')) * ureg.eV
                 norb = self.data.get('.config').attrs.get('atoms.1.nd')
                 for subkey in self._inequivalent_atom_map.keys():
                     parameters = []

tests/test_soliddmft.py
@@ -100,7 +100,7 @@ def test_srvo3(parser):
     assert sec_gfs.self_energy_iw.dtype == 'complex128'
     assert sec_gfs.self_energy_iw.shape == (1, 2, 3, 1002)
     assert sec_gfs.greens_function_tau[0][1][1][1025] == approx(-0.14109113749664728 + 0j)
-    assert sec_gfs.chemical_potential.magnitude == approx(0.0378235342396917)
+    assert sec_gfs.chemical_potential.to('eV').magnitude == approx(0.0378235342396917)
     assert np.sum(sec_gfs.orbital_occupations) == approx(1.008321846227956)
     assert sec_gfs.quasiparticle_weights.shape == (1, 2, 3)
     assert sec_gfs.quasiparticle_weights[0][0][0] == approx(0.16195076915540912)
@@ -108,5 +108,5 @@ def test_srvo3(parser):
     sec_scf = sec_scc.scf_iteration
     assert len(sec_scf) == 3
     assert sec_scf[0].energy.fermi.to('eV').magnitude == approx(-0.027041)
-    assert sec_scf[-1].energy.fermi.to('eV').magnitude == approx(sec_gfs.chemical_potential.magnitude)
+    assert sec_scf[-1].energy.fermi.to('eV').magnitude == approx(sec_gfs.chemical_potential.to('eV').magnitude)
     assert sec_scf[1].x_soliddmft_convergence_obs.x_soliddmft_d_G0[0] == 0.03463529305810029