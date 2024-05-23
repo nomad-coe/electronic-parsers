8c7d293622a420d445e2d6ca585c01c11c534eb4
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-23 14:16:50 +0200

    use not None to check for truthiness

--- electronicparsers/atk/parser.py
+++ electronicparsers/atk/parser.py
@@ -326,12 +326,8 @@ class ATKParser:
             sec_atoms.lattice_vectors = atoms.get_cell().array * ureg.angstrom
             sec_atoms.periodic = atoms.get_pbc()
             velocities = atoms.get_velocities()
-            if isinstance(velocities, np.ndarray):
-                if velocities.any():
-                    sec_atoms.velocities = velocities * (ureg.angstrom / ureg.fs)
-            else:
-                if velocities:
-                    sec_atoms.velocities = velocities * (ureg.angstrom / ureg.fs)
+            if velocities is not None:
+                sec_atoms.velocities = velocities * (ureg.angstrom / ureg.fs)
 
             return sec_system
 
