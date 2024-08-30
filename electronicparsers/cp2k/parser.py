5a81e2a46e2f6f80cceb4a6817c1d8884b6c7669
nathan <n.daelman@hotmail.com>
2024-08-30 14:10:25 +0200

    Add logging warnings for improper termination or process id handling

--- electronicparsers/cp2k/parser.py
+++ electronicparsers/cp2k/parser.py
@@ -967,7 +967,7 @@ class CP2KParser:
             'started_at': 'start_time',
             'started_on': 'start_host',
             'started_by': 'start_user',
-            'process_id': 'id',
+            'process_id': 'process_id',
             'started_in': 'start_path',
             'ended_at': 'end_time',
             'ran_on': 'end_host',
@@ -1481,15 +1481,15 @@ class CP2KParser:
             sec_run.x_cp2k_section_end_information.append(sec_endinformation)
             section = sec_startinformation
             for key, val in program_settings.items():
-                if key == 'id':
+                if key == 'process_id':
                     if isinstance(val, list):
                         sec_startinformation.x_cp2k_start_id = val[0]
                         sec_endinformation.x_cp2k_end_id = val[1]
                     elif isinstance(val, int):
-                        # logger.warning('Calculation may not have properly terminated: did not encounter end "PROCESS ID".')
+                        self.logger.warning('Calculation may not have properly terminated: did not encounter end "PROCESS ID".')
                         sec_startinformation.x_cp2k_start_id = val
                     else:
-                        # logger.warning('Encountered "PROCESS ID" of unexpected format.')
+                        self.logger.warning('Encountered "PROCESS ID" of unexpected format.')
                         pass
                     continue
                 section = (
