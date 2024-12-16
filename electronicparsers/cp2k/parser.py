537906b355a1e3b3fefaf2b7ac8b424936767b85
ndaelman <ndaelman@physik.hu-berlin.de>
2024-12-16 17:04:14 +0100

    Fix logic loophole when no inp files are recuperated

--- electronicparsers/cp2k/parser.py
+++ electronicparsers/cp2k/parser.py
@@ -1570,25 +1570,24 @@ class CP2KParser:
         input_files = get_files(
             input_filename, self.filepath, self.mainfile, deep=False
         )
-        if not input_files:
-            self.logger.warning(
-                'Input *.inp file not found. We will attempt finding the restart file from '
-                'the project_name appending a -1, <project_name>-1.restart.',
-                data={'project_name': project_name},
-            )
-            # Patch to check if the input is .restart and CP2K appended a -1 after the name
+        if not input_files:  # ? what is the purpose of this recovery logic
+            self.logger.warning('Input *.inp file not found.')
+
             if project_name:
+                self.logger.warning(
+                    f' We will attempt finding the restart file from {project_name}-1.restart'
+                )
                 project_filename = f'{project_name}-1.restart'
                 input_files = get_files(
                     project_filename, self.filepath, self.mainfile, deep=False
                 )
+            elif len(input_files) > 1:
+                self.logger.warning(
+                    f'Multiple input files found. We will parse the first file retrieved.'
+                )  # ! TODO: employ better heuristic OR parse all
+                self.inp_parser.mainfile = input_files[0]
             else:
                 return
-        if len(input_files) > 1:
-            self.logger.warning(
-                f'Multiple input files found. We will parse the first read file.'
-            )
-        self.inp_parser.mainfile = input_files[0]
 
         parse('x_cp2k_section_input', self.inp_parser.tree, self.archive.run[-1])
 
